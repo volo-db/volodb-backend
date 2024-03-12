4e948b70b5055f8cdc1056a86af534927d6f1b1d
Fabian Urner <fabian.urner+gh@gmail.com>
2024-03-12 13:51:35 +0100

    fix: changed endpoint for Contracts from /contract to /contracts

--- src/main/java/dev/urner/volodb/rest/ProjectRestController.java
+++ src/main/java/dev/urner/volodb/rest/ProjectRestController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/project")
+@RequestMapping("/api/v1/projects")
 @RequiredArgsConstructor
 public class ProjectRestController {
 
