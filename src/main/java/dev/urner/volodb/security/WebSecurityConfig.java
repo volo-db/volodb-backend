a5842e243de87cf396a0362d1faf5784c15bd41d
Fabian Urner <fabian.urner+gh@gmail.com>
2024-03-14 21:46:00 +0100

    fix: CorsConfig now in WebSecurityConfig

--- src/main/java/dev/urner/volodb/security/WebSecurityConfig.java
+++ src/main/java/dev/urner/volodb/security/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package dev.urner.volodb.security;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,22 +27,31 @@ public class WebSecurityConfig {
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
   private final CustomUserDetailService customUserDetailService;
 
-
   @Bean
   SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
     http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     http
-      .cors(AbstractHttpConfigurer::disable)
-      .csrf(AbstractHttpConfigurer::disable)
-      .sessionManagement( s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-      .formLogin(AbstractHttpConfigurer::disable)
-      .securityMatcher("/**")
-      .authorizeHttpRequests(registry -> registry
-        .requestMatchers("/auth/login").permitAll()
-        .requestMatchers("/error").permitAll()
-        .anyRequest().authenticated()
-        );
+        .cors(cors -> {
+          cors.configurationSource(request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOriginPatterns(Arrays.asList("*"));
+            config.setAllowedHeaders(Arrays.asList("*"));
+            config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH",
+                "DELETE"));
+            config.setAllowCredentials(true);
+            return config;
+          });
+        })
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .formLogin(AbstractHttpConfigurer::disable)
+        .securityMatcher("/**")
+        .authorizeHttpRequests(registry -> registry
+            .requestMatchers("/auth/login").permitAll()
+            .requestMatchers("/error").permitAll()
+            .anyRequest()
+            .authenticated());
 
     return http.build();
   }
@@ -51,12 +65,10 @@ public class WebSecurityConfig {
   public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
     var builder = http.getSharedObject(AuthenticationManagerBuilder.class);
     builder
-      .userDetailsService(customUserDetailService)
-      .passwordEncoder(passwordEncoder());
-    
+        .userDetailsService(customUserDetailService)
+        .passwordEncoder(passwordEncoder());
+
     return builder.build();
   }
-
-
 
 }
