3309a9f14a109e99362c1047e3440c0c369177a4
Fabian Urner <fabian.urner+gh@gmail.com>
2024-05-15 09:34:40 +0200

    fix: hotfix

--- src/main/java/dev/urner/volodb/VolodbApplication.java
+++ src/main/java/dev/urner/volodb/VolodbApplication.java
@@ -1,30 +1,13 @@
 package dev.urner.volodb;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class VolodbApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(VolodbApplication.class, args);
-
   }
 
-}
-
-@Component
-class MyApplicationRunner implements ApplicationRunner {
-
-  @Value("${volodb.minio.test}")
-  private String testVal;
-
-  @Override
-  public void run(ApplicationArguments args) throws Exception {
-    System.out.println("✨✨✨✨✨: " + testVal);
-  }
 }
