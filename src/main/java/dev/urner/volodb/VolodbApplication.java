757cbf1591efafdb2ebd295aea028a7b55a17916
Fabian Urner <fabian.urner+gh@gmail.com>
2024-05-15 09:28:03 +0200

    fix: testing environment-vars

--- src/main/java/dev/urner/volodb/VolodbApplication.java
+++ src/main/java/dev/urner/volodb/VolodbApplication.java
@@ -1,7 +1,11 @@
 package dev.urner.volodb;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class VolodbApplication {
@@ -11,4 +15,16 @@ public class VolodbApplication {
 
   }
 
+}
+
+@Component
+class MyApplicationRunner implements ApplicationRunner {
+
+  @Value("${volodb.minio.test}")
+  private String testVal;
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    System.out.println("✨✨✨✨✨: " + testVal);
+  }
 }
