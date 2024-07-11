18b43c8b00208ce921e64fd8fb7adcb9b777761e
Fabian Urner <fabian.urner+gh@gmail.com>
2024-07-11 10:20:54 +0200

    fix: syntax-errors

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -427,8 +427,8 @@ VALUES
   (1, 'Koalick', 'Björn'),/*_______12*/
   (1, 'Edenhofer', 'Martina'),/*___13*/
   (1, 'Goldsmits', 'Mike'),/*______14*/
-  (1, 'Mohring', 'Erik');/*________15*/
-  (4, 'Senger', 'Ari');/*__________16*/
+  (1, 'Mohring', 'Erik'),/*________15*/
+  (4, 'Senger', 'Ari'),/*__________16*/
   (4, 'Schubert', 'Jessica');/*____17*/
 
 INSERT INTO country (name, iso2, iso3, local_name, continent, nationality)
@@ -731,9 +731,9 @@ INSERT INTO user (person, email, secret, organisational_role, avatar)
 VALUES
   (6, 'wiesinger@donbosco.de', '$2a$12$bvG0ScICxF6gaF4cstczNub7PI79w/o0EIez9dlUoZe70eMP.eaL2', 'Fachreferentin für Freiwilligendienste', 'volodb/default-files/default-avatar.png'),
   (7, 'jakob.bopp@donbosco.de', '$2a$12$bvG0ScICxF6gaF4cstczNub7PI79w/o0EIez9dlUoZe70eMP.eaL2', 'Fachreferent für Freiwilligendienste', 'volodb/default-files/default-avatar.png'),
-  (8, 'urner@donbosco.de', '$2a$12$bvG0ScICxF6gaF4cstczNub7PI79w/o0EIez9dlUoZe70eMP.eaL2', 'Fachreferent für Freiwilligendienste', 'volodb/default-files/default-avatar.png');
-  (8, 'fabi@volodb.de', '$2a$12$bvG0ScICxF6gaF4cstczNub7PI79w/o0EIez9dlUoZe70eMP.eaL2', 'VoloDB Entwickler:in', 'volodb/default-files/default-avatar.png');
-  (16, 'ari@volodb.de', '$2a$12$bvG0ScICxF6gaF4cstczNub7PI79w/o0EIez9dlUoZe70eMP.eaL2', 'VoloDB Entwickler:in', 'volodb/default-files/default-avatar.png');
+  (8, 'urner@donbosco.de', '$2a$12$bvG0ScICxF6gaF4cstczNub7PI79w/o0EIez9dlUoZe70eMP.eaL2', 'Fachreferent für Freiwilligendienste', 'volodb/default-files/default-avatar.png'),
+  (8, 'fabi@volodb.de', '$2a$12$bvG0ScICxF6gaF4cstczNub7PI79w/o0EIez9dlUoZe70eMP.eaL2', 'VoloDB Entwickler:in', 'volodb/default-files/default-avatar.png'),
+  (16, 'ari@volodb.de', '$2a$12$bvG0ScICxF6gaF4cstczNub7PI79w/o0EIez9dlUoZe70eMP.eaL2', 'VoloDB Entwickler:in', 'volodb/default-files/default-avatar.png'),
   (17, 'jessi@volodb.de', '$2a$12$bvG0ScICxF6gaF4cstczNub7PI79w/o0EIez9dlUoZe70eMP.eaL2', 'VoloDB Entwickler:in', 'volodb/default-files/default-avatar.png');
 
 INSERT INTO user_role (name)
